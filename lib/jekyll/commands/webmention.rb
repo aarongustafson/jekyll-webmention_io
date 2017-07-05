10f4598240039bf45e73eccd613c52a2a612dd90
Aaron Gustafson <aaron@easy-designs.net>
2017-07-05 14:17:41 -0400

    Spaces

--- lib/jekyll/commands/webmention.rb
+++ lib/jekyll/commands/webmention.rb
@@ -5,44 +5,44 @@ module Jekyll
         prog.command(:webmention) do |c|
           c.syntax 'webmention'
           c.description 'Sends queued webmentions'
-					
-					c.action { |args, options| process args, options }
+          
+          c.action { |args, options| process args, options }
         end
       end
 
       def self.process( args=[], options={} )
-				cached_outgoing = Jekyll::WebmentionIO::get_cache_file_path 'outgoing'
-				cached_sent     = Jekyll::WebmentionIO::get_cache_file_path 'sent'
-				if File.exists?(cached_outgoing)
-					if File.exists?(cached_sent)
-						sent = open(cached_sent) { |f| YAML.load(f) }
-					else
-						sent = {}
-					end  # file exists (sent)
-					outgoing = open(cached_outgoing) { |f| YAML.load(f) }
-					outgoing.each_pair do |source, targets|
-						if ! sent[source] or ! sent[source].kind_of? Array
-							sent[source] = Array.new
-						end
-						targets.each do |target|
-							if target and ! sent[source].find_index( target )
-								if target.index( "//" ) == 0
-									target  = "http:#{target}"
-								end
-								endpoint = Jekyll::WebmentionIO::get_webmention_endpoint( target )
-								if endpoint
-									endpoint.scan(/href="([^"]+)"/) do |endpoint_url|
-										endpoint_url = endpoint_url[0]
-										Jekyll::WebmentionIO::webmention( source, target, endpoint )
-									end
-									sent[source].push( target )
-								end
-							end
-						end
-					end
-					File.open(cached_sent, 'w') { |f| YAML.dump(sent, f) }
-				end # file exists (outgoing)
-			end # def process
+        cached_outgoing = Jekyll::WebmentionIO::get_cache_file_path 'outgoing'
+        cached_sent     = Jekyll::WebmentionIO::get_cache_file_path 'sent'
+        if File.exists?(cached_outgoing)
+          if File.exists?(cached_sent)
+            sent = open(cached_sent) { |f| YAML.load(f) }
+          else
+            sent = {}
+          end  # file exists (sent)
+          outgoing = open(cached_outgoing) { |f| YAML.load(f) }
+          outgoing.each_pair do |source, targets|
+            if ! sent[source] or ! sent[source].kind_of? Array
+              sent[source] = Array.new
+            end
+            targets.each do |target|
+              if target and ! sent[source].find_index( target )
+                if target.index( "//" ) == 0
+                  target  = "http:#{target}"
+                end
+                endpoint = Jekyll::WebmentionIO::get_webmention_endpoint( target )
+                if endpoint
+                  endpoint.scan(/href="([^"]+)"/) do |endpoint_url|
+                    endpoint_url = endpoint_url[0]
+                    Jekyll::WebmentionIO::webmention( source, target, endpoint )
+                  end
+                  sent[source].push( target )
+                end
+              end
+            end
+          end
+          File.open(cached_sent, 'w') { |f| YAML.dump(sent, f) }
+        end # file exists (outgoing)
+      end # def process
     end # WebmentionCommand
   end # Commands
 end # Jekyll
