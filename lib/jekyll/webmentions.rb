b4001892fc0461c2ae1d5691c2eefb6fa4e40d92
Brett Kosinski <brettk@b-ark.ca>
2025-10-03 21:43:21 -0600

    Fix some Rubocop warnings
    I mostly use the relaxed settings but I do like the string style
    enforcement...

--- lib/jekyll/webmentions.rb
+++ lib/jekyll/webmentions.rb
@@ -7,7 +7,7 @@ module Jekyll
     class Webmentions
       attr_writer :api_suffix
 
-      def initialize(policy, client = NetworkClient.new, url = "https://webmention.io/api", path = "mentions", suffix = "&perPage=9999")
+      def initialize(policy, client = NetworkClient.new, url = 'https://webmention.io/api', path = 'mentions', suffix = '&perPage=9999')
         @policy = policy
         @client = client
 
@@ -18,25 +18,25 @@ module Jekyll
       def send_webmention(source, target)
         return nil if !webmention_endpoint?(URI::Parser.new.escape(target))
 
-        Jekyll::WebmentionIO.log "info", "Sending webmention of #{target} in #{source}"
+        Jekyll::WebmentionIO.log 'info', "Sending webmention of #{target} in #{source}"
         # return `curl -s -i -d \"source=#{source}&target=#{target}\" -o /dev/null #{endpoint}`
         response = @client.send_webmention(source, target)
 
         case response.code
         when 200, 201, 202
-          Jekyll::WebmentionIO.log "info", "Webmention successful!"
+          Jekyll::WebmentionIO.log 'info', 'Webmention successful!'
           @policy.success(target)
           response.body
         else
-          Jekyll::WebmentionIO.log "info", response.inspect
-          Jekyll::WebmentionIO.log "info", "Webmention failed, but will remain queued for next time"
+          Jekyll::WebmentionIO.log 'info', response.inspect
+          Jekyll::WebmentionIO.log 'info', 'Webmention failed, but will remain queued for next time'
 
           if response.body
             begin
               body = JSON.parse(response.body)
 
-              if body.key? "error"
-                Jekyll::WebmentionIO.log "msg", "Endpoint returned error: #{body['error']}"
+              if body.key? 'error'
+                Jekyll::WebmentionIO.log 'msg', "Endpoint returned error: #{body['error']}"
               end
             rescue
             end
@@ -49,18 +49,18 @@ module Jekyll
 
       # Returns WebmentionIO::WebmentionItem instances
       def get_webmentions(targets, since_id)
-        api_params = targets.collect { |v| "target[]=#{v}" }.join("&")
+        api_params = targets.collect { |v| "target[]=#{v}" }.join('&')
         api_params << "&since_id=#{since_id}" if since_id
-        api_params << "&sort-by=published"
+        api_params << '&sort-by=published'
 
         response = get_webmention_io_response(api_params)
 
-        links = response["links"] || []
+        links = response['links'] || []
 
         if links.empty?
-          WebmentionIO.log "info", "No webmentions found."
+          WebmentionIO.log 'info', 'No webmentions found.'
         else
-          WebmentionIO.log "info", "Here’s what we got back:\n\n#{response.inspect}\n\n"
+          WebmentionIO.log 'info', "Here’s what we got back:\n\n#{response.inspect}\n\n"
         end
 
         links.reverse.map { |wm| WebmentionIO::WebmentionItem.new(link) }
@@ -88,11 +88,11 @@ module Jekyll
           endpoint = @client.webmention_endpoint(uri)
 
           unless endpoint
-            Jekyll::WebmentionIO.log("info", "Could not find a webmention endpoint at #{uri}")
+            Jekyll::WebmentionIO.log('info', "Could not find a webmention endpoint at #{uri}")
             @policy.unsupported(uri)
           end
         rescue StandardError => e
-          Jekyll::WebmentionIO.log "info", "Endpoint lookup failed for #{uri}: #{e.message}"
+          Jekyll::WebmentionIO.log 'info', "Endpoint lookup failed for #{uri}: #{e.message}"
           @policy.failure(uri)
           endpoint = nil
         end
@@ -103,7 +103,7 @@ module Jekyll
       def get_webmention_io_response(api_params)
         api_params << @api_suffix
         url = URI::Parser.new.escape("#{@api_endpoint}?#{api_params}")
-        Jekyll::WebmentionIO.log "info", "Sending request to #{url}."
+        Jekyll::WebmentionIO.log 'info', "Sending request to #{url}."
         source = get_body_from_uri(url)
         if source
           JSON.parse(source)
