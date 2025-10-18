14e82647a707837ec86e15030c035f8e39d28d94
Brett Kosinski <brettk@b-ark.ca>
2025-10-18 10:46:31 -0600

    Modify these tests to use rspec mocks
    Let's get rid of all this custom mock business and just use RSpec
    infrastructure.

--- spec/gather_webmentions_spec.rb
+++ spec/gather_webmentions_spec.rb
@@ -1,163 +1,205 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'securerandom'
 require 'timecop'
 
 describe Jekyll::WebmentionIO::GatherWebmentions do
+  let(:config) { double('Jekyll::WebmentionIO::Config') }
+  let(:caches) { double('Jekyll::WebmentionIO::Caches') }
+  let(:webmentions) { instance_double('Jekyll::WebmentionIO::Webmentions') }
+  let(:site) { instance_double('Jekyll::Site') }
+  let(:policy) { double('Jekyll::WebmentionIO::WebmentionPolicy') }
+  let(:incoming_webmentions_cache) { spy('Cache') }
+  let(:site_lookups_cache) { spy('Cache') }
+
   before do
     Jekyll.logger.log_level = :error
 
-    @config = SpecHelper::MockConfig.new
-    @caches = SpecHelper::MockCaches.new
-    @webmentions = SpecHelper::MockWebmentions.new
+    allow(Jekyll::WebmentionIO).to receive(:config).and_return(config)
+    allow(Jekyll::WebmentionIO).to receive(:caches).and_return(caches)
+    allow(Jekyll::WebmentionIO).to receive(:webmentions).and_return(webmentions)
+    allow(Jekyll::WebmentionIO).to receive(:site).and_return(site)
+    allow(Jekyll::WebmentionIO).to receive(:policy).and_return(policy)
 
-    # Just an arbitrary, non-leap-year (for simplicity) start date for various date-based tests
-    @epoch = DateTime.new(2002, 1, 17)
+    allow(caches).to receive(:incoming_webmentions).and_return(incoming_webmentions_cache)
+    allow(caches).to receive(:site_lookups).and_return(site_lookups_cache)
 
-    Jekyll::WebmentionIO.bootstrap(nil, @config, @caches, nil, @webmentions)
+    allow(config).to receive(:pause_lookups).and_return(false)
+    allow(config).to receive(:legacy_domains).and_return([])
+    allow(config).to receive(:throttle_lookups).and_return({})
+    allow(config).to receive(:site_url).and_return('https://example.com')
 
-    @generator = Jekyll::WebmentionIO::GatherWebmentions.new
+    allow(webmentions).to receive(:get_webmentions).and_return([])
+    allow(webmentions).to receive(:get_body_from_uri).and_return('')
+
+    allow(policy).to receive(:post_should_be_throttled?).and_return(false)
   end
 
-  it 'gathers a single webmention' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz')
+  let(:generator) { described_class.new }
 
-    @config.documents.append(page)
-    wm = @webmentions.add(uri: page.uri, source: 'http://yadda.yadda.yadda')
+  context 'when gathering webmentions' do
+    let(:page) { double('Jekyll::Page', url: '/page.html', data: {}, uri: 'https://example.com/page.html') }
+    let(:webmention) do
+      item = Jekyll::WebmentionIO::WebmentionItem.new({ 'id' => '12345', 'source' => 'http://source.com', 'target' => page.uri, 'data' => {} })
+      allow(item).to receive(:to_hash).and_return({ 'id' => '12345' })
+      item
+    end
 
-    @generator.generate
+    before do
+      allow(config).to receive(:documents).and_return([page])
+      allow(webmentions).to receive(:get_webmentions).and_return([webmention])
+      allow(incoming_webmentions_cache).to receive(:dig).and_return(nil)
+      allow(site_lookups_cache).to receive(:[]).and_return(nil)
+    end
 
-    expect(@webmentions.last_targets).to contain_exactly(page.uri)
-    expect(@caches.incoming_webmentions).to match(page.url => { wm.id => wm.to_hash })
+    it 'gathers a single webmention' do
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).with([page.uri], anything)
+      expect(incoming_webmentions_cache).to have_received(:[]=).with(page.url, anything)
+    end
+
+    it 'handles a redirect' do
+      allow(page).to receive(:data).and_return({ 'redirect_from' => '/redirect.html' })
+      redirect_uri = 'https://example.com/redirect.html'
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).with([page.uri, redirect_uri], anything)
+    end
+
+    it 'handles legacy domain' do
+      allow(config).to receive(:legacy_domains).and_return(['http://legacy.com'])
+      legacy_uri = 'http://legacy.com/page.html'
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).with([page.uri, legacy_uri], anything)
+    end
+
+    it 'requests new webmentions' do
+      allow(incoming_webmentions_cache).to receive(:dig).with(page.url, anything).and_return({ 'raw' => { 'id' => '54321', 'verified_date' => '2025-10-18' } })
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).with([page.uri], '54321')
+    end
   end
 
-  it 'handles a redirect' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', data: { 'redirect_from' => 'hello.world' })
+  context 'when throttling lookups' do
+    let(:page) { double('Jekyll::Page', url: '/page.html', data: {}, uri: 'https://example.com/page.html', date: DateTime.now) }
 
-    @config.documents.append(page)
-    wm = @webmentions.add(uri: page.redirect, source: 'http://yadda.yadda.yadda')
+    before do
+      allow(config).to receive(:documents).and_return([page])
+      allow(incoming_webmentions_cache).to receive(:dig).and_return(nil)
+    end
 
-    @generator.generate
+    it 'throttles posts from last week daily' do
+      allow(config).to receive(:throttle_lookups).and_return({ 'last_week' => 'daily' })
+      allow(page).to receive(:date).and_return(DateTime.now - 7)
 
-    expect(@webmentions.last_targets).to contain_exactly(page.uri, page.redirect)
-    expect(@caches.incoming_webmentions).to match(page.url => { wm.id => wm.to_hash })
-  end
+      # Initial state
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(nil)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(false)
 
-  it 'handles legacy domain' do
-    @config.legacy_domains << 'http://legacy.domain'
+      # First run, should not be throttled
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once
 
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz')
-    legacy_uri = File.join('http://legacy.domain', page.url)
+      # Second run, should be throttled
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(true)
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once # still once
 
-    @config.documents.append(page)
-    wm = @webmentions.add(uri: legacy_uri, source: 'http://yadda.yadda.yadda')
+      # Third run, next day, should not be throttled
+      Timecop.travel(Date.today + 1) do
+        allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+        allow(policy).to receive(:post_should_be_throttled?).and_return(false)
+        generator.generate(site)
+        expect(webmentions).to have_received(:get_webmentions).twice
+      end
+    end
 
-    @generator.generate
+    it 'throttles posts from last month weekly' do
+      allow(config).to receive(:throttle_lookups).and_return({ 'last_month' => 'weekly' })
+      allow(page).to receive(:date).and_return(DateTime.now - 30)
 
-    expect(@webmentions.last_targets).to contain_exactly(page.uri, legacy_uri)
-    expect(@caches.incoming_webmentions).to match(page.url => { wm.id => wm.to_hash })
-  end
+      # Initial state
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(nil)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(false)
 
-  it 'requests new webmentions' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', data: { 'redirect_from' => 'hello.world' })
+      # First run, should not be throttled
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once
 
-    @config.documents.append(page)
-    wm = @webmentions.add(uri: page.redirect, source: 'http://yadda.yadda.yadda')
+      # Second run, should be throttled
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(true)
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once # still once
 
-    @generator.generate
+      # Third run, next week, should not be throttled
+      Timecop.travel(Date.today + 7) do
+        allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+        allow(policy).to receive(:post_should_be_throttled?).and_return(false)
+        generator.generate(site)
+        expect(webmentions).to have_received(:get_webmentions).twice
+      end
+    end
 
-    expect(@webmentions.last_since_id).to eq(false)
+    it 'throttles posts from last year every two weeks' do
+      allow(config).to receive(:throttle_lookups).and_return({ 'last_year' => 'every 2 weeks' })
+      allow(page).to receive(:date).and_return(DateTime.now - 365)
 
-    @webmentions.clear
+      # Initial state
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(nil)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(false)
 
-    @generator.generate
+      # First run, should not be throttled
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once
 
-    expect(@webmentions.last_since_id).to eq(wm.id)
-  end
+      # Second run, should be throttled
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(true)
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once # still once
 
-  it 'throttles posts from last week daily' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', date: @epoch - 7)
+      # Third run, 2 weeks later, should not be throttled
+      Timecop.travel(Date.today + 14) do
+        allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+        allow(policy).to receive(:post_should_be_throttled?).and_return(false)
+        generator.generate(site)
+        expect(webmentions).to have_received(:get_webmentions).twice
+      end
+    end
 
-    @config.documents.append(page)
-    @config.throttle_lookups = { 'last_week' => 'daily' }
+    it 'throttles old posts monthly' do
+      allow(config).to receive(:throttle_lookups).and_return({ 'older' => 'monthly' })
+      allow(page).to receive(:date).and_return(DateTime.now - 400)
 
-    Timecop.freeze(@epoch) { @generator.generate }
+      # Initial state
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(nil)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(false)
 
-    @webmentions.clear
-    Timecop.freeze(@epoch) { @generator.generate }
+      # First run, should not be throttled
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once
 
-    expect(@webmentions.last_targets).to be_empty
+      # Second run, should be throttled
+      allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+      allow(policy).to receive(:post_should_be_throttled?).and_return(true)
+      generator.generate(site)
+      expect(webmentions).to have_received(:get_webmentions).once # still once
 
-    @webmentions.clear
-    Timecop.freeze(@epoch + 1) { @generator.generate }
-
-    expect(@webmentions.last_targets.count).to eq(1)
-  end
-
-  it 'throttles posts from last month weekly' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', date: DateTime.new(2001, 12, 31))
-
-    @config.documents.append(page)
-    @config.throttle_lookups = { 'last_month' => 'weekly' }
-
-    Timecop.freeze(@epoch) { @generator.generate }
-
-    @webmentions.clear
-    Timecop.freeze(@epoch + 6) { @generator.generate }
-
-    expect(@webmentions.last_targets).to be_empty
-
-    @webmentions.clear
-    Timecop.freeze(@epoch + 7) { @generator.generate }
-
-    expect(@webmentions.last_targets.count).to eq(1)
-  end
-
-  it 'throttles posts from last year every two weeks' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', date: DateTime.new(2001, 6, 1))
-
-    @config.documents.append(page)
-    @config.throttle_lookups = { 'last_year' => 'every 2 weeks' }
-
-    Timecop.freeze(@epoch) { @generator.generate }
-
-    @webmentions.clear
-    Timecop.freeze(DateTime.new(2002, 1, 30)) { @generator.generate }
-
-    expect(@webmentions.last_targets).to be_empty
-
-    @webmentions.clear
-    Timecop.freeze(DateTime.new(2002, 1, 31)) { @generator.generate }
-
-    expect(@webmentions.last_targets.count).to eq(1)
-  end
-
-  it 'throttles old posts monthly' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz', date: DateTime.new(2000, 6, 1))
-
-    @config.documents.append(page)
-    @config.throttle_lookups = { 'older' => 'monthly' }
-
-    Timecop.freeze(@epoch) { @generator.generate }
-
-    @webmentions.clear
-    Timecop.freeze(DateTime.new(2002, 2, 16)) { @generator.generate }
-
-    expect(@webmentions.last_targets).to be_empty
-
-    @webmentions.clear
-    Timecop.freeze(DateTime.new(2002, 2, 17)) { @generator.generate }
-
-    expect(@webmentions.last_targets.count).to eq(1)
+      # Third run, next month, should not be throttled
+      Timecop.travel(Date.today + 30) do
+        allow(site_lookups_cache).to receive(:[]).with(page.url).and_return(Date.today)
+        allow(policy).to receive(:post_should_be_throttled?).and_return(false)
+        generator.generate(site)
+        expect(webmentions).to have_received(:get_webmentions).twice
+      end
+    end
   end
 
   it 'honours pause_lookups setting' do
-    page = SpecHelper::MockPage.new(url: 'foo.bar.baz')
-
-    @config.documents.append(page)
-    @config.pause_lookups = true
-
-    @generator.generate
-
-    expect(@webmentions.last_targets).to be_empty
+    allow(config).to receive(:pause_lookups).and_return(true)
+    generator.generate(site)
+    expect(webmentions).not_to have_received(:get_webmentions)
   end
 end
