17c1fc2635f2c9421fa3bf58942ea6b81e65c43a
Brett Kosinski <brettk@b-ark.ca>
2025-10-19 10:37:49 -0600

    Clean up legacy tests
    Really narrowing the scope of the code in spec_helper, here, since this
    code is no longer used anywhere else in the test suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,46 +5,6 @@ require 'jekyll-webmention_io'
 require 'html-proofer'
 require 'json'
 
-Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require f }
-
-
-def dest_dir
-  File.expand_path("../tmp/dest", __dir__)
-end
-
-def source_dir
-  File.expand_path("fixtures", __dir__)
-end
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 ENV['JEKYLL_LOG_LEVEL'] = 'error'
-
-CONFIG_DEFAULTS = {
-  "source"      => source_dir,
-  "destination" => dest_dir,
-  "gems"        => ["jekyll-webmention_io"],
-}.freeze
-
-module SpecHelper
-  def self.make_page(options = {})
-    page = Jekyll::Page.new site, CONFIG_DEFAULTS["source"], "", "page.md"
-    page.data = options
-    page
-  end
-
-  def self.make_post(options = {})
-    filename = File.expand_path("_posts/2001-01-01-post.md", CONFIG_DEFAULTS["source"])
-    config = { :site => site, :collection => site.collections["posts"] }
-    page = Jekyll::Document.new filename, config
-    page.merge_data!(options)
-    page
-  end
-
-  def self.make_site(options = {})
-    config = Jekyll.configuration CONFIG_DEFAULTS.merge(options)
-    Jekyll::Site.new(config)
-  end
-
-  def self.make_context(registers = {}, environments = {})
-    Liquid::Context.new(environments, {}, { :site => site, :page => page }.merge(registers))
-  end
-end
