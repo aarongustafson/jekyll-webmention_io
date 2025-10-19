7033693f2ad25eb538b1ce14c3ecaeb51af9ba5e
Brett Kosinski <brettk@b-ark.ca>
2025-10-19 10:07:50 -0600

    Initial refactoring of webmention command UTs
    Continuing the theme of switching to RSpec mocking infra over our hand
    rolled stuff.

--- spec/webmention_command_spec.rb
+++ spec/webmention_command_spec.rb
@@ -1,247 +1,169 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'timecop'
 
 describe Jekyll::WebmentionIO::Commands::WebmentionCommand do
-  NetworkClient = Jekyll::WebmentionIO::NetworkClient
+  let(:config) { Jekyll::WebmentionIO::Config.new }
+  let(:caches) { instance_double(Jekyll::WebmentionIO::Caches) }
+  let(:outgoing_webmentions) { {} }
+  let(:bad_uris) { {} }
+  let(:webmentions) { instance_double(Jekyll::WebmentionIO::Webmentions) }
+  let(:policy) { Jekyll::WebmentionIO::WebmentionPolicy.new(config, caches) }
+  let(:command) { Jekyll::WebmentionIO::Commands::WebmentionCommand }
 
   before do
     Jekyll.logger.log_level = :error
 
-    @config = SpecHelper::MockConfig.new
-    @caches = SpecHelper::MockCaches.new
-    @command = Jekyll::WebmentionIO::Commands::WebmentionCommand
+    allow(Jekyll::WebmentionIO).to receive(:config).and_return(config)
+    allow(Jekyll::WebmentionIO).to receive(:caches).and_return(caches)
+    allow(Jekyll::WebmentionIO).to receive(:webmentions).and_return(webmentions)
+    allow(Jekyll::WebmentionIO).to receive(:policy).and_return(policy)
+
+    allow(caches).to receive(:outgoing_webmentions).and_return(outgoing_webmentions)
+    allow(caches).to receive(:bad_uris).and_return(bad_uris)
+    allow(outgoing_webmentions).to receive(:write)
+    allow(bad_uris).to receive(:write)
   end
 
-  context 'with mock webmentions class' do
-    before do
-      @webmentions = SpecHelper::MockWebmentions.new
-
-      Jekyll::WebmentionIO.bootstrap(nil, @config, @caches, nil, @webmentions)
-    end
-
-    # This method adds an outgoing webmention to the cache and sets up a
-    # response object in our mock.
-    def set_up_webmention(source, target, body = false)
-      response = { 'success' => true }
-
-      @caches.outgoing_webmentions[source] = { target => body }
-      @webmentions.response(source, target, response)
-
-      response
-    end
+  context 'sends webmentions' do
+    let(:source) { 'http://foo.bar.baz' }
+    let(:target) { 'http://www.test.com' }
+    let(:response) { { 'success' => true } }
 
     it 'sends basic webmention' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      response = set_up_webmention(source, target)
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => response } })
+      outgoing_webmentions[source] = { target => false }
+      allow(webmentions).to receive(:send_webmention).with(source, target).and_return(response.to_json)
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => response } })
     end
 
     it 'skips webmentions that have a response' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      set_up_webmention(source, target, {})
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => {} } })
+      outgoing_webmentions[source] = { target => {} }
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => {} } })
     end
 
     it 'skips protocol-less links (1)' do
-      source = 'http://foo.bar.baz'
-      target = 'www.test.com'
-
-      set_up_webmention(source, target)
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => false } })
+      outgoing_webmentions[source] = { 'www.test.com' => false }
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { 'www.test.com' => false } })
     end
 
     it 'skips protocol-less links (2)' do
-      source = 'http://foo.bar.baz'
-      target = '//www.test.com'
-
-      set_up_webmention(source, target)
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => false } })
+      outgoing_webmentions[source] = { '//www.test.com' => false }
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { '//www.test.com' => false } })
     end
   end
 
-  context 'with mock network client' do
-    before do
-      @client = SpecHelper::MockNetworkClient.new
-      policy = Jekyll::WebmentionIO::WebmentionPolicy.new(@config, @caches)
-      @webmentions = Jekyll::WebmentionIO::Webmentions.new(policy, @client)
+  context 'handles webmention sending failures' do
+    let(:source) { 'http://foo.bar.baz' }
+    let(:target) { 'http://www.test.com' }
+    let(:client) { instance_double(Jekyll::WebmentionIO::NetworkClient) }
+    let(:webmentions) { Jekyll::WebmentionIO::Webmentions.new(policy, client) }
 
-      Jekyll::WebmentionIO.bootstrap(nil, @config, @caches, policy, @webmentions)
+    before do
+      allow(Jekyll::WebmentionIO).to receive(:webmentions).and_return(webmentions)
     end
 
     it 'handles failure getting endpoint' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 1 } })
-      expect(@caches.bad_uris).to match(hash_including(
-        'www.test.com' => hash_including('state' => 'failure', 'attempts' => 1)
-      ))
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 1 } })
+      expect(bad_uris).to match(hash_including(
+                                  'www.test.com' => hash_including('state' => 'failure', 'attempts' => 1)
+                                ))
     end
 
     it 'handles webmentions unsupported' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = nil
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 1 } })
-      expect(@caches.bad_uris).to match(hash_including(
-        'www.test.com' => hash_including('state' => 'unsupported', 'attempts' => 1)
-      ))
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_return(nil)
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 1 } })
+      expect(bad_uris).to match(hash_including(
+                                  'www.test.com' => hash_including('state' => 'unsupported', 'attempts' => 1)
+                                ))
     end
 
     it 'handles webmention failure' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = target
-      @client.webmention_responses[target] = { source => Struct.new(:code, :body).new(404) }
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 1 } })
-      expect(@caches.bad_uris).to match(hash_including(
-        'www.test.com' => hash_including('state' => 'error', 'attempts' => 1)
-      ))
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_return(target)
+      allow(client).to receive(:send_webmention).with(source, target).and_return(Struct.new(:code, :body).new(404))
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 1 } })
+      expect(bad_uris).to match(hash_including(
+                                  'www.test.com' => hash_including('state' => 'error', 'attempts' => 1)
+                                ))
     end
 
     it 'handles ignore on failure' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-      @config.bad_uri_policy.set_policy('failure', 'ignore')
-
-      @command.send_webmentions
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 2 } })
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => 'ignore' } })
+      command.send_webmentions
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 2 } })
     end
 
     it 'handles ban on failure' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-      @config.bad_uri_policy.set_policy('failure', 'ban')
-
-      @command.send_webmentions
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 1 } })
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => 'ban' } })
+      command.send_webmentions
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 1 } })
     end
 
     it 'handles target in blacklist' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = { webmention: target }
-      @client.webmention_responses[target] = { source => Struct.new(:code, :body).new(200, "{}") }
-      @config.bad_uri_policy.blacklist << target
-
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => false } })
+      outgoing_webmentions[source] = { target => false }
+      config.parse({ 'bad_uri_policy' => { 'blacklist' => [target] } })
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => false } })
     end
 
     it 'handles target in whitelist' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-      @config.bad_uri_policy.set_policy('failure', 'ban')
-      @config.bad_uri_policy.whitelist << target
-
-      @command.send_webmentions
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 2 } })
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => 'ban', 'whitelist' => [target] } })
+      command.send_webmentions
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 2 } })
     end
 
     it 'handles retry while inside delay period' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-
-      # Set a 1 hour retry period, and retry immediately
-      @config.bad_uri_policy.set_policy('failure', 'retry', 2, [1])
-
-      @command.send_webmentions
-      @command.send_webmentions
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 1 } })
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => { 'policy' => 'retry', 'max_attempts' => 2, 'retry_delay' => [1] } } })
+      command.send_webmentions
+      command.send_webmentions
+      expect(outgoing_webmentions).to match({ source => { target => 1 } })
     end
 
     it 'handles retry outside delay period' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
-
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
-
-      # Set a 1 hour retry period, and retry immediately
-      @config.bad_uri_policy.set_policy('failure', 'retry', 2, [1])
-
-      @command.send_webmentions
-
-      Timecop.freeze(DateTime.now + 1/24r) { @command.send_webmentions }
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 2 } })
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => { 'policy' => 'retry', 'max_attempts' => 2, 'retry_delay' => [1] } } })
+      command.send_webmentions
+      Timecop.freeze(DateTime.now + (1.0 / 24.0)) { command.send_webmentions }
+      expect(outgoing_webmentions).to match({ source => { target => 2 } })
     end
 
     it 'handles retry with multiple delay periods' do
-      source = 'http://foo.bar.baz'
-      target = 'http://www.test.com'
+      outgoing_webmentions[source] = { target => false }
+      allow(client).to receive(:webmention_endpoint).with(target).and_raise(StandardError.new('No bueno'))
+      config.parse({ 'bad_uri_policy' => { 'failure' => { 'policy' => 'retry', 'max_attempts' => 5, 'retry_delay' => [1, 2] } } })
 
-      @caches.outgoing_webmentions[source] = { target => false }
-      @client.endpoint_responses[target] = StandardError.new('No bueno')
+      command.send_webmentions
+      Timecop.freeze(DateTime.now + (1.0 / 24.0)) { command.send_webmentions }
+      Timecop.freeze(DateTime.now + (2.0 / 24.0)) { command.send_webmentions }
+      Timecop.freeze(DateTime.now + (3.0 / 24.0)) { command.send_webmentions }
+      Timecop.freeze(DateTime.now + (4.0 / 24.0)) { command.send_webmentions }
+      Timecop.freeze(DateTime.now + (5.0 / 24.0)) { command.send_webmentions }
+      Timecop.freeze(DateTime.now + (6.0 / 24.0)) { command.send_webmentions }
 
-      # Set a 1 hour retry period, and retry immediately
-      @config.bad_uri_policy.set_policy('failure', 'retry', 5, [1, 2])
-
-      @command.send_webmentions
-
-      Timecop.freeze(DateTime.now + 1/24r) { @command.send_webmentions }
-      Timecop.freeze(DateTime.now + 2/24r) { @command.send_webmentions }
-
-      Timecop.freeze(DateTime.now + 3/24r) { @command.send_webmentions }
-      Timecop.freeze(DateTime.now + 4/24r) { @command.send_webmentions }
-
-      Timecop.freeze(DateTime.now + 5/24r) { @command.send_webmentions }
-      Timecop.freeze(DateTime.now + 6/24r) { @command.send_webmentions }
-
-      expect(@caches.outgoing_webmentions).to match({ source => { target => 4 } })
+      expect(outgoing_webmentions).to match({ source => { target => 4 } })
     end
   end
 end
